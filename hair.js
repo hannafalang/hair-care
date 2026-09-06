4094ca617657f2ac84aee038a83ee044dc5f9d2a
hannafalang04 <hfalang@gmail.com>
2026-09-06 19:37:41 +0200

    la til ds text 2

--- hair.js
+++ hair.js
@@ -1,14 +1,24 @@
 const textElement = document.getElementById("typing-text");
+const textElement2 = document.getElementById("typing-text2");
 const textToType = "this website will help you understand what products are best for your hair type!";
+const textToType2 = "I want you to answer the following questions:";
+
 let index = 0;
+let index2 = 0;
 
 function typeWriter() {
   if (index < textToType.length) {
-    textElement.innerHTML += textToType.charAt(index);
+    textElement.textContent += textToType.charAt(index);
     index++;
-    setTimeout(typeWriter, 100); // Speed of typing in milliseconds (100ms per letter)
+    setTimeout(typeWriter, 50);
+    return;
+  }
+
+  if (index2 < textToType2.length) {
+    textElement2.textContent += textToType2.charAt(index2);
+    index2++;
+    setTimeout(typeWriter, 50);
   }
 }
 
-// Start the animation when the page loads
 window.onload = typeWriter;
